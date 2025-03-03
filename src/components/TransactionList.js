ad60b834826d0caf3c698e98c3f7d05c889682f4
Johny <Johny@test.com>
2025-03-03 18:35:29 +0000

    Id Tags Added

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,18 +1,18 @@
-import React, { useContext } from 'react'
-import {GlobalContext} from '../context/GlobalState'
-import {Transaction} from './Transaction'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import { Transaction } from './Transaction';
 
 export const TransactionList = () => {
-    const {transactions} = useContext(GlobalContext);
+    const { transactions } = useContext(GlobalContext);
     
     return (
         <>
-          <h3>History</h3>
-          <ul className="list">
-              {transactions.map(transaction => ( <Transaction key={transaction.id} transaction={transaction}/>))}
-              
-
-            </ul>  
+            <h3 id="transaction-history-title">History</h3>
+            <ul id="transaction-list" className="list">
+                {transactions.map(transaction => (
+                    <Transaction key={transaction.id} transaction={transaction} />
+                ))}
+            </ul>
         </>
-    )
-}
+    );
+};
